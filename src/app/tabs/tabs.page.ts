5589ff3a80be66533d0c55e26e9a13344364decb
abhijitshirke07 <145590550+abhijitshirke07@users.noreply.github.com>
2024-04-23 12:28:18 +0530

    Add files via upload

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {personAddOutline, personAddSharp, peopleOutline, peopleSharp} from 'ionicons/icons'
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss'],
+  // imports: [personAddOutline, personAddSharp]
+})
+export class TabsPage {
+
+  constructor() {}
+
+}
